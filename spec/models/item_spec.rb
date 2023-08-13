8bf024ca30ce30d7150f6d7f2601ad39ac507b0f
shampooEarl24 <take.sleepr24@gmail.com>
2023-08-13 21:31:50 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.create(:item)
+    @item = FactoryBot.build(:item)
   end
 
   describe '商品の出品登録' do
