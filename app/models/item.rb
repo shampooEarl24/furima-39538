7fb5e981dc5904eeb6679d9db80db81bc4a6ff5e
shampooEarl24 <take.sleepr24@gmail.com>
2023-08-13 19:10:00 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -28,7 +28,7 @@ class Item < ApplicationRecord
     validates :delivery_area_id
     validates :shipping_date_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   end
 
   # ジャンルの選択が「--」の時は保存不可
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -121,6 +121,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = 'abc123' # 半角数字以外を含む価格
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
     end
   end
 end
