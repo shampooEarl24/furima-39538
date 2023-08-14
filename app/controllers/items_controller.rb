621b6c443adebdaa40e809b6cbe19afa150554fe
shampooEarl24 <take.sleepr24@gmail.com>
2023-08-14 17:50:02 +0900

    詳細

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: :show
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -17,10 +18,16 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+  end
   
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :delivery_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
