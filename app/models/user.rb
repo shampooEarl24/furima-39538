b8d77efe998f7490d67543dfc525b3ad9c586add
shampooEarl24 <take.sleepr24@gmail.com>
2023-08-22 17:00:21 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :items
+         has_many :orders
          with_options presence: true do
           # 存在すること・確認用を含めて2回入力・6字以上はdeviseのデフォルト実装のため省略
           # 半角英数字（空文字NG）以外の場合には、メッセージを出す
